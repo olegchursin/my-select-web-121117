3c4fe24f4e94fc9b0d4eb07d802c05a5dc109578
Oleg Chursin <olegchursin@gmail.com>
2017-11-29 06:54:07 +0000

    automatically backed up by learn

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,10 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  new_array = []
+ i = 0
+ while i < array.length
+   if yield(array[i])
+     new_array << array[i]
+   i += 1
+ end
+ new_array
 end
