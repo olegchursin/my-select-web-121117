e64bdc8f9e4f9e6cacdeafbf4156e2b5df4e2777
Oleg Chursin <olegchursin@gmail.com>
2017-11-29 06:57:21 +0000

    Done.

--- lib/my_select.rb
+++ lib/my_select.rb
@@ -1,3 +1,29 @@
-def my_select(collection)
- # your code here!
+def my_select(array)
+  new_array = []
+ i = 0
+ while i < array.length
+   if yield(array[i])
+     new_array << array[i]
+   end
+   i += 1
+ end
+ new_array
 end
+
+
+
+#not working
+# def my_select(array)
+#   new_array = []
+#  i = 0
+#  while i < array.length
+#    new_array.push yield(array[i])
+#    i += 1
+#  end
+#  new_array
+# end
+#
+# nums = []
+# my_select(nums) do |num|
+#   p num.even?
+# end
